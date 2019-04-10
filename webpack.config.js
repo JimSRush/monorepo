7f835be864d4e3f9de233068072072e42b49178f
Jim Rush <jim@gettimely.com>
2019-04-10 21:16:44 +1200

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "module": {
+        "rules": [
+            {
+                "enforce": "pre",
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": "eslint-loader"
+            },
+            {
+                "test": /\.js$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "env"
+                        ]
+                    }
+                }
+            },
+            {
+                "test": /\.css$/,
+                "use": [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+  };
