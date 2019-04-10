2c25692f9b01af9d87e3b429488d73bbf7ffdf50
Jim Rush <jim@gettimely.com>
2019-04-11 09:54:59 +1200

    Updated stories to add text knob

--- packages/widebar/__stories__/Widebar.stories.js
+++ packages/widebar/__stories__/Widebar.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf, addDecorator } from '@storybook/react';
 import Widebar from '../src';
-import { select, withKnobs } from '@storybook/addon-knobs';
+import { select, text, withKnobs } from '@storybook/addon-knobs';
 
 addDecorator(withKnobs);
 
@@ -9,7 +9,7 @@ storiesOf('Widebar', module)
   .add('with text', () => {
     const type = select('Type', ["TYPE_BLACK", 'TYPE_ALERT'], 'TYPE_BLACK');
     return <Widebar 
-      text={"Some stuff happened today"}
+      text={text("Text:", "Something happened today")}
       type={type}
     />
   });
